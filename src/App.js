27dfec419806f1fbd9c6fceb96ab6f27ebd23d72
parkjungkwan <pakjkwan@gmail.com>
2021-01-22 10:17:32 +0900

    10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Login from './cmm/Login'
-
+/* import Login from './cmm/Login' */
+import BasicRouter from './tst/BasicRouter'
 export default function App() {
   return (
-    <Login/>
+    <BasicRouter/>
   )
 }
 
