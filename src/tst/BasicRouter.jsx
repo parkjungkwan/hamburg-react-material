cbcf5e8894f372a0b99d93d2f4d2b6b8f604ca20
parkjungkwan <pakjkwan@gmail.com>
2021-01-22 10:45:42 +0900

    10

--- src/tst/BasicRouter.jsx
+++ src/tst/BasicRouter.jsx
@@ -3,15 +3,24 @@ import { BrowserRouter as Router,
 Switch, Route, Link} from 'react-router-dom'
 
 export default function BasicRouter(){
-    return (<>
+    return (<Router>
     <div style={{border: '1px solid black', width: '400px', margin: '100px auto', }}>
         <nav>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Users</li>
+                <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/users'>Users</Link></li>
             </ul>
         </nav>
+        <Switch>
+            <Route path='/home'><Home/></Route>
+            <Route path='/about'><About/></Route>
+            <Route path='/users'><Users/></Route>
+        </Switch>
     </div>
-    </>)
+    </Router>)
 }
+
+function Home(){return (<h1>Home</h1>)}
+function About(){return (<h1>About</h1>)}
+function Users(){return (<h1>Users</h1>)}
