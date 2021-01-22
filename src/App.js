2c60df6690700582bc961a500bc9526f1e1d7dbe
parkjungkwan <pakjkwan@gmail.com>
2021-01-22 12:30:32 +0900

    12

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Blog from './bbs/pages/Blog'
+/* import Blog from './bbs/pages/Blog' */
+/* import Album from './bbs/pages/Album' */
 /* import BasicRouter from './tst/BasicRouter' */
-
+/* import Checkout from './sts/pages/Checkout' */
+/* import Dashboard from './sts/pages/Dashboard' */
+import SignIn from './sts/pages/Dashboard'
 export default function App() {
   return (
-    <Blog/>
+    <Dashboard/>
   )
 }
 
