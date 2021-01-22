b73fc165086d6a17c809108423a76fef06c75d5f
parkjungkwan <pakjkwan@gmail.com>
2021-01-22 12:47:46 +0900

    12

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 /* import BasicRouter from './tst/BasicRouter' */
 /* import Checkout from './sts/pages/Checkout' */
 /* import Dashboard from './sts/pages/Dashboard' */
-import SignIn from './sts/pages/Dashboard'
+/* import SignInSide from './sym/pages/SignInSide' */
+/* import Pricing from './uss/pages/Pricing' */
+/* import SiginUp from './uss/pages/StdnJoin' */
+import Home from './cmm/pages/Home'
 export default function App() {
   return (
-    <Dashboard/>
+    <Home/>
   )
 }
 
